1b3bc5cf544cc25138e5371a3f32b68758b2ebf4
bmalbright <brianalbright@utexas.edu>
2022-03-31 01:07:34 -0500

    updated player code. added player.js. changed block to @ symbol.

--- src/Rogue.js
+++ src/Rogue.js
@@ -1,15 +1,16 @@
 import React, {useRef, useEffect, useState } from 'react';
 import InputManager from './ImputManager';
+import Player from './Player';
 
 const Rogue = ({width, height, tilesize}) => {
     const canvasRef = useRef();
-    const [player, setPlayer] = useState({x:64, y:128});
+    const [player, setPlayer] = useState(new Player(1, 2, tilesize));
     let inputManager = new InputManager();
     const handleInput = (action, data) => {
         console.log(`handle input: ${action}:${JSON.stringify(data)}`);
-        let newPlayer = { ...player};
-        newPlayer.x += data.x * tilesize;
-        newPlayer.y += data.y * tilesize;
+        let newPlayer = new Player();
+        Object.assign(newPlayer, player);
+        newPlayer.move(data.x, data.y);
         setPlayer(newPlayer);
     };
 
@@ -27,8 +28,7 @@ useEffect(() => {
         console.log('Draw to canvas');
         const context = canvasRef.current.getContext('2d');
         context.clearRect(0,0, width * tilesize, height * tilesize);
-        context.fillStyle='#000';
-        context.fillRect(player.x, player.y, 16, 16);
+        player.draw(context);
     });
     return(
     <canvas 
